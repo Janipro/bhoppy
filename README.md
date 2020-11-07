fbe5e2f1bc94b0f57f05cbb52ed32dede706edae
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2020-11-07 19:24:31 +0100

    Improve performance

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Bhoppy
 A Bunny Hopper for **CS:GO**. <br>
-Updated for [07.11.2020](https://blog.counter-strike.net/index.php/2020/10/31827/).
+Working as of [07.11.2020](https://blog.counter-strike.net/index.php/2020/10/31827/).
--- src/bhoppy.py
+++ src/bhoppy.py
@@ -6,6 +6,7 @@
 import time
 import keyboard
 import pymem.process
+from win32gui import GetWindowText, GetForegroundWindow
 
 if not keyboard:
     print("Keyboard has not been imported!")
@@ -15,6 +16,9 @@ if not time:
 
 if not pymem:
     print("Pymem has not been imported!")
+
+if not GetWindowText or GetForegroundWindow:
+    print("Win32gui has not been imported correctly!")
 
 # First we need to load the CS:GO memory offsets. These might change after an update.
 dwForceJump = 0x51FBFA8
@@ -28,14 +32,18 @@ while True:
     if keyboard.is_pressed("delete"):
         raise SystemExit
 
-    if keyboard.is_pressed("space"):
-        jump = client + dwForceJump
-        player_status = game.read_int(client + dwLocalPlayer)
-        if player_status:
+    try:
+        name = GetWindowText(GetForegroundWindow())
+        if game.read_int(client + dwLocalPlayer) and name == "Counter-Strike: Global Offensive":
+            player_status = game.read_int(client + dwLocalPlayer)
+            jump = client + dwForceJump
             on_ground = game.read_int(player_status + m_fFlags)
-            if on_ground & (1 << 0):
+            if keyboard.is_pressed('space') and on_ground == 257:
                 game.write_int(jump, 5)
-                time.sleep(.07)
+                time.sleep(0.15)
                 game.write_int(jump, 4)
+    except:
+        pass
+
 
 
